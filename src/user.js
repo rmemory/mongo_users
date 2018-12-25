c174e3dfc4c50a047a383221dc51f239e481b216
Richard Memory <richardfmemory@gmail.com>
2018-12-24 18:49:19 -0700

    Add basic create, find, and remove capabilities

--- src/user.js
+++ src/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// User schema
 const UserSchema = new Schema({
 	name: String
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,45 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
-mongoose.connection
-	.once('open', () => console.log("Connected to \"users_test\" mongo database"))
-	.on('error', (error) => {
-		console.warn('Warning', error)
-	});
+mongoose.Promise = global.Promise; // Use ES6 promises
+
+before((done) => {
+	mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
+	mongoose.connection
+		.once('open', () => {
+			console.log("Successfully connected to database; Initialization complete");
+			console.log("Starting tests ...");
+			done(); // Make sure connection occurs before tests run 
+		})
+		.on('error', (error) => {
+			console.warn('Warning', error);
+			process.exit(-1);
+		});
+});
+
+beforeEach((done) => {
+	const { users, comments, blogposts } = mongoose.connection.collections;
+	if (users) {
+		users.drop(() => {
+			if (comments) {
+				comments.drop(() => {
+					if (blogposts) {
+						blogposts.drop(() => {
+							done();
+						});	
+					} else {
+						done();
+					}
+				});
+			} else {
+				done();
+			}
+		});
+	} else {
+		done();
+	}
+});
+
+after((done) => {
+	console.log("Tests complete");
+	done();
+})
