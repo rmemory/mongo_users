14e90c931d65b2dbfbd876f3587d4e7172e96cfd
Richard Memory <richardfmemory@gmail.com>
2018-12-28 14:24:57 -0700

    Add user data validation

--- src/user.js
+++ src/user.js
@@ -3,7 +3,14 @@ const Schema = mongoose.Schema;
 
 // User schema
 const UserSchema = new Schema({
-	name: String,
+	name: {
+		type: String,
+		validate: {
+			validator: (name) => name.length > 2,
+			message: 'Name must be longer than 2 characters.'
+		},
+		required: [true, 'Name is required.'],
+	},
 	postCount: Number,
 });
 
